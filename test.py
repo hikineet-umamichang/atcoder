0b88899b096a5e95208968483d79f63acbdb7ca6
hikineet-umamichang <1308yuta@gmail.com>
2023-05-28 13:26:30 +0900

    abc303

--- test.py
+++ test.py
@@ -1,11 +1,2 @@
-n, k = map(int, input().split())
-a = list(map(int, input().split()))
-
-b = []
-
-for l in range(n - 1):
-    for r in range(l + 1, n):
-        b.append(a[:l] + a[r:l:-1] + a[r:])
-b.sort()
-
-print(*b[k - 1])
+n=int(input())
+t,a=map(int,)
